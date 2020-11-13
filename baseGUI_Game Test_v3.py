893d82944af17b175deb21870c5a6bde20349053
dianamichellefischer <73234217+dianamichellefischer@users.noreply.github.com>
2020-11-13 07:42:36 -0600

    Add files via upload
    Updated file; still needs to be able to pick 2 random cards to "deal" from the deck, those are hard coded right now

---
+++ baseGUI_Game Test_v3.py
@@ -0,0 +1,73 @@
+"""
+File: CH9_P9.py
+"""
+
+from breezypythongui import EasyFrame
+from tkinter import PhotoImage
+from cards import Deck, Card
+import os
+
+class dealCards(EasyFrame):
+    """Illustrates command"""
+
+    def __init__(self):
+        """Sets up the GUI"""
+        EasyFrame.__init__(self, title = "Game Time")
+        self.setSize(440, 400)
+        self.cardLabel1 = self.addLabel("", row = 0,
+                                       column = 0,
+                                       sticky = "NSEW")
+        self.cardLabel2 = self.addLabel("", row = 0,
+                                       column = 1,
+                                       sticky = "NSEW")
+        self.cardLabel3 = self.addLabel("", row = 0,
+                                       column = 2,
+                                       sticky = "NSEW")
+        self.stateLabel = self.addLabel("", row = 1, column = 0,
+                                        sticky = "NSEW",
+                                        columnspan = 2)
+        self.addButton(row = 2, column = 0,
+                       text = "New game",
+                       command = self.newDeal)
+        self.addButton(row = 2, column = 2,
+                       text = "Quit",
+                       command = self.quit)
+        
+
+    def newDeal(self):
+        """new deal shows 2 cards from the deck and the back of a card/the deck"""
+        self.card1 = Card(1, 'c')
+        self.card2 = Card(4, 'd')
+        self.stateLabel["text"] = ""
+        self.refreshImages()
+
+       
+
+    def exit(self):
+        return False
+
+    def refreshImages(self):
+        """brings images from the DECK folder"""
+        fileName1 = "DECK/" + str(self.card1) + ".gif"
+        fileName2 = "DECK/" + str(self.card2) + ".gif"
+        fileName3 = "DECK/" + str('b') + ".gif"
+        self.image1 = PhotoImage(file = fileName1)
+        self.cardLabel1["image"] = self.image1
+        self.image2 = PhotoImage(file = fileName2)
+        self.cardLabel2["image"] = self.image2
+        self.image3 = PhotoImage(file = fileName3)
+        self.cardLabel3["image"] = self.image3
+
+    
+ 
+
+
+
+
+
+def main():
+    """Instantiate and pop up the window."""
+    dealCards().mainloop()
+
+if __name__ == "__main__":
+    main()
